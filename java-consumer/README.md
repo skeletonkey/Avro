91d3a8e31cd2fa4d173adfdee495ca76632fe81d
Erik Tank <erik@jundy.com>
2019-08-16 16:37:22 -0700

    Java Consumer

---
+++ java-consumer/README.md
@@ -0,0 +1,31 @@
+# Java Consumer 
+
+
+## Build & Develop
+```
+docker build --rm=false -t java_consumer .
+docker run -it --rm --name my_java_consumer --network host java_consumer /bin/bash
+```
+
+To get share pertinent files between host and container:
+
+```
+docker run -it --rm --name my_java_consumer --netork host -v $(pwd)/code:/code java_consumer /bin/bash
+```
+
+## Consume from Kafka Topic
+It's assumed that Confluent Platform is running using `example/cp-all-in-one`.
+
+Also, that there's a topic named `config_user_rules`.
+
+Run the container - it'll consume every record from the topic each time it is run:
+
+```
+docker run -it --rm --name my_java_consumer --network host java_consumer
+```
+
+Run the container (choose your favorite way from the `Build & Develop` section) and run:
+
+```
+mvn -q exec:java -Dexec.mainClass=com.jundy.avro.App
+```
---
+++ java-consumer/code/src/main/java/com/jundy/avro/App.java
@@ -0,0 +1,52 @@
+package com.jundy.avro;
+
+import java.util.*;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+import com.jundy.client.Rules;
+import org.apache.kafka.common.TopicPartition;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        String topicName = "config_user_rules";
+
+        Properties props = new Properties();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "avro_presentation");
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "io.confluent.kafka.serializers.KafkaAvroDeserializer");
+        props.put("schema.registry.url", "http://localhost:8081");
+
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        System.out.println("Trying to connect to the consumer");
+        try (KafkaConsumer<String, Rules> consumer = new KafkaConsumer<>(props)) {
+            System.out.println("Subscribe to " + topicName);
+            consumer.subscribe(Arrays.asList(topicName));
+
+            //https://stackoverflow.com/questions/41008610/kafkaconsumer-0-10-java-api-error-message-no-current-assignment-for-partition
+            // "Also keep in mind, that subscribe() and assign() are lazy -- thus, you also need to do a "dummy call" to poll() before you can use seek()."
+            ConsumerRecords<String, Rules> recordsToBeIgnored = consumer.poll(100);
+
+            consumer.seekToBeginning(new TopicPartition(topicName, 0));
+
+            while (true) {
+//                System.out.println("Poll ....");
+                ConsumerRecords<String, Rules> records = consumer.poll(100);
+                for (ConsumerRecord<String, Rules> record : records) {
+                    System.out.println("Record Received !!!!!");
+                    System.out.println("Offset: " + record.offset());
+                    System.out.println(record.key());
+                    System.out.println(record.value());
+                }
+            }
+        }
+    }
+}
